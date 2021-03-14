1cd419a72666756b97359da86720289022f934e8
Juergen Adams <juergen@jekyll.one>
2021-03-14 20:40:13 +0100

    Made Asciidoctor adapter regenerated

--- packages/400_template_site/conum.rb
+++
@@ -1,9 +0,0 @@
-Asciidoctor::Extensions.register do
-  inline_macro do
-    named :conum
-    process do |parent, target, attrs|
-      # TODO validate that this conum is valid
-      Asciidoctor::Inline.new(parent, :callout, target.to_i).convert
-    end
-  end
-end
