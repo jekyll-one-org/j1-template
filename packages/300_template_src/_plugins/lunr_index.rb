8cedb1e01efee9c634316380198c562a58b70bd2
Juergen Adams <juergen@jekyll.one>
2021-09-14 19:01:24 +0200

    New version 2021.1.23

--- packages/300_template_src/_plugins/lunr_index.rb
+++ packages/300_template_src/_plugins/lunr_index.rb
@@ -364,6 +364,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2021.1.22'
+    VERSION = '2021.1.23'
   end
 end
--- packages/500_template_gem/lib/j1/version.rb
+++ packages/500_template_gem/lib/j1/version.rb
@@ -1,3 +1,3 @@
 module J1
-  VERSION = '2021.1.22'
+  VERSION = '2021.1.23'
 end
