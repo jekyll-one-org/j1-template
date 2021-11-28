0102e858050d02156d4727267c34507572ed805f
Juergen Adams <juergen@jekyll.one>
2021-11-28 18:38:31 +0100

    New version 2022.0.2

--- packages/300_template_src/_plugins/lunr_index.rb
+++ packages/300_template_src/_plugins/lunr_index.rb
@@ -364,6 +364,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2022.0.1'
+    VERSION = '2022.0.2'
   end
 end
--- packages/500_template_gem/lib/j1/version.rb
+++ packages/500_template_gem/lib/j1/version.rb
@@ -1,3 +1,3 @@
 module J1
-  VERSION = '2022.0.1'
+  VERSION = '2022.0.2'
 end
