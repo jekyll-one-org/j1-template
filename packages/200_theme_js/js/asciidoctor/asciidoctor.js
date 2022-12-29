214c01f837eb68c6874a008b8d4ea3702495adbd
Juergen Adams <juergen@jekyll.one>
2022-12-29 18:43:41 +0100

    Updated year (2023) in copyright for all JS sources

--- packages/200_theme_js/js/asciidoctor/asciidoctor.js
+++ packages/200_theme_js/js/asciidoctor/asciidoctor.js
@@ -6,7 +6,7 @@
  #  Product/Info:
  #  http://jekyll.one
  #
- #  Copyright (C) 2022 Juergen Adams
+ #  Copyright (C) 2023 Juergen Adams
  #
  #  J1 Theme is licensed under MIT License.
  #  See: https://github.com/jekyll-one/J1 Theme/blob/master/LICENSE
@@ -30,7 +30,7 @@
 // -----------------------------------------------------------------------------
 /*!
  * J1 Asciidoctor
- * Copyright (C) 2022 Juergen Adams
+ * Copyright (C) 2023 Juergen Adams
  * Licensed under MIT License.
  */
 module.exports = function asciidoctor (options) {
--- packages/200_theme_js/js/tocbot/tocbot.js
+++ packages/200_theme_js/js/tocbot/tocbot.js
@@ -8,7 +8,7 @@
  #  https://tscanlin.github.io/tocbot
  #  https://github.com/tscanlin/tocbot
  #
- #  Copyright (C) 2022 Juergen Adams
+ #  Copyright (C) 2023 Juergen Adams
  #  Copyright (C) 2016 Tim Scanlin
  #
  #  J1 Theme is licensed under MIT License.
