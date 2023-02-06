7ebf8d8bfa241d1d0f1d30bb8b290fdb92a828b1
Juergen Adams <juergen@jekyll.one>
2023-02-06 22:33:59 +0100

    Prepare new version 2023.0.12

--- packages/300_theme_src/_plugins/index/lunr.rb
+++ packages/300_theme_src/_plugins/index/lunr.rb
@@ -391,6 +391,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2023.0.11'
+    VERSION = '2023.0.12'
   end
 end
--- packages/500_theme_gem/lib/j1/version.rb
+++ packages/500_theme_gem/lib/j1/version.rb
@@ -1,3 +1,3 @@
 module J1
-  VERSION = '2023.0.11'
+  VERSION = '2023.0.12'
 end
