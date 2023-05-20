0a6f06b2104e38f7e16416f1b91d6d730051a25f
Juergen Adams <juergen@jekyll.one>
2023-05-20 22:58:56 +0200

    Added module "iconPicker"

--- packages/300_theme_src/assets/themes/j1/adapter/js/iconPicker.js
+++ packages/300_theme_src/assets/themes/j1/adapter/js/iconPicker.js
@@ -6,8 +6,8 @@ regenerate:                             true
 
 {% comment %}
  # -----------------------------------------------------------------------------
- # ~/assets/themes/j1/adapter/js/waves.js
- # Liquid template to adapt the Waves module
+ # ~/assets/themes/j1/adapter/js/iconPicker.js
+ # Liquid template to adapt the iconPicker module
  #
  # Product/Info:
  # https://jekyll.one
@@ -18,7 +18,7 @@ regenerate:                             true
  # -----------------------------------------------------------------------------
  # Test data:
  #  {{ liquid_var | debug }}
- #  wave_options:  {{ wave_options | debug }}
+ #  icon_picker_options:  {{ icon_picker_options | debug }}
  # -----------------------------------------------------------------------------
 {% endcomment %}
 
@@ -27,30 +27,29 @@ regenerate:                             true
 
 {% comment %} Set global settings
 -------------------------------------------------------------------------------- {% endcomment %}
-{% assign environment       = site.environment %}
-{% assign asset_path        = "/assets/themes/j1" %}
+{% assign environment           = site.environment %}
+{% assign asset_path            = "/assets/themes/j1" %}
 
 {% comment %} Process YML config data
 ================================================================================ {% endcomment %}
 
 {% comment %} Set config files
 -------------------------------------------------------------------------------- {% endcomment %}
-{% assign template_config    = site.data.j1_config %}
-{% assign blocks             = site.data.blocks %}
-{% assign modules            = site.data.modules %}
+{% assign template_config       = site.data.j1_config %}
+{% assign blocks                = site.data.blocks %}
+{% assign modules               = site.data.modules %}
 
 {% comment %} Set config data (settings only)
 -------------------------------------------------------------------------------- {% endcomment %}
-{% assign wave_defaults = modules.defaults.waves.defaults %}
-{% assign wave_settings = modules.waves.settings %}
+{% assign icon_picker_defaults  = modules.defaults.iconPicker.defaults %}
+{% assign icon_picker_settings  = modules.iconPicker.settings %}
 
 {% comment %} Set config options (settings only)
 -------------------------------------------------------------------------------- {% endcomment %}
-{% assign wave_options  = wave_defaults | merge: wave_settings %}
+{% assign icon_picker_options   = icon_picker_defaults | merge: icon_picker_settings %}
 
 {% comment %} Variables
 -------------------------------------------------------------------------------- {% endcomment %}
-{% assign comments          = wave_options.enabled %}
 
 {% comment %} Detect prod mode
 -------------------------------------------------------------------------------- {% endcomment %}
@@ -61,8 +60,8 @@ regenerate:                             true
 
 /*
  # -----------------------------------------------------------------------------
- # ~/assets/themes/j1/adapter/js/waves.js
- # J1 Adapter for the waves module
+ # ~/assets/themes/j1/adapter/js/iconPicker.js
+ # J1 Adapter for the iconPicker module
  #
  # Product/Info:
  # https://jekyll.one
@@ -72,7 +71,7 @@ regenerate:                             true
  # J1 Theme is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
  # -----------------------------------------------------------------------------
- # NOTE: Wave styles defind in /assets/data/panel.html, key 'wave'
+ # NOTE:
  # -----------------------------------------------------------------------------
  #  Adapter generated: {{site.time}}
  # -----------------------------------------------------------------------------
@@ -84,21 +83,15 @@ regenerate:                             true
 /* eslint indent: "off"                                                       */
 // -----------------------------------------------------------------------------
 'use strict';
-j1.adapter.waves = (function (j1, window) {
+j1.adapter.iconPicker = (function (j1, window) {
 
 {% comment %} Set global variables
 -------------------------------------------------------------------------------- {% endcomment %}
 var environment     = '{{environment}}';
-var cookie_names    = j1.getCookieNames();
-var user_state      = j1.readCookie(cookie_names.user_state);
-var viewport_width  = $(window).width();
-var waveDefaults;
-var waveSettings;
-var waveOptions;
+var iconPickerDefaults;
+var iconPickerSettings;
+var iconPickerOptions;
 var frontmatterOptions;
-var themes_allowed;
-var theme_enabled;
-var theme;
 var _this;
 var logger;
 var logText;
@@ -114,14 +107,11 @@ var logText;
     // -------------------------------------------------------------------------
     init: function (options) {
 
-      // [INFO   ] [j1.adapter.comments                    ] [ detected comments provider (j1_config): {{comments_provider}}} ]
-      // [INFO   ] [j1.adapter.comments                    ] [ start processing load region head, layout: {{page.layout}} ]
-
       // -----------------------------------------------------------------------
       // Default module settings
       // -----------------------------------------------------------------------
       var settings = $.extend({
-        module_name: 'j1.adapter.waves',
+        module_name: 'j1.adapter.iconPicker',
         generated:   '{{site.time}}'
       }, options);
 
@@ -130,16 +120,15 @@ var logText;
       // -----------------------------------------------------------------------
 
       // create settings object from frontmatter
-      frontmatterOptions  = options != null ? $.extend({}, options) : {};
+      frontmatterOptions = options != null ? $.extend({}, options) : {};
 
       // create settings object from module options
-      waveDefaults = $.extend({}, {{wave_defaults | replace: 'nil', 'null' | replace: '=>', ':' }});
-      waveSettings = $.extend({}, {{wave_settings | replace: 'nil', 'null' | replace: '=>', ':' }});
-      waveOptions  = $.extend(true, {}, waveDefaults, waveSettings, frontmatterOptions);
+      iconPickerDefaults = $.extend({}, {{icon_picker_defaults | replace: 'nil', 'null' | replace: '=>', ':' }});
+      iconPickerSettings = $.extend({}, {{icon_picker_settings | replace: 'nil', 'null' | replace: '=>', ':' }});
+      iconPickerOptions  = $.extend(true, {}, iconPickerDefaults, iconPickerSettings, frontmatterOptions);
 
-      _this  = j1.adapter.waves;
-      theme  = user_state.theme_name;
-      logger = log4javascript.getLogger('j1.adapter.wave');
+      _this  = j1.adapter.iconPicker;
+      logger = log4javascript.getLogger('j1.adapter.iconPicker');
 
       // -----------------------------------------------------------------------
       // initializer
@@ -150,38 +139,28 @@ var logText;
         var atticFinished = (j1.adapter.attic.getState() == 'finished') ? true: false;
 
         if (j1.getState() === 'finished' && pageVisible && atticFinished) {
-          themes_allowed = waveOptions.themes.toString();
-          theme_enabled  = waveOptions.themes.indexOf(theme) > -1 ? true : false;
 
           _this.setState('started');
           logger.debug('\n' + 'state: ' + _this.getState());
           logger.info('\n' + 'module is being initialized');
 
-          logger.debug('\n' + 'themes allowd: ' + themes_allowed);
-          logger.debug('\n' + 'theme detected: ' + theme);
+          var icon_picker = new UniversalIconPicker('#selector', {
+            iconLibraries: [
+              'font-awesome.min.json',
+              'mdi-icons.min.json'
+            ],
+            iconLibrariesCss: [
+              'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+//            '/assets/themes/j1/core/css/icon-fonts/mdi.min.css'
+              'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.2.96/css/materialdesignicons.min.css'
+            ],
+            onSelect: function(jsonIconData) {
+              document.getElementById('output').innerHTML = jsonIconData.iconMarkup;
+              console.log(jsonIconData);
+            }
+          });
 
-          // TODO: Check why a timeout is required to enable|disable the wave elements
-          if (themes_allowed === 'all' ) {
-            logger.info('\n' + 'activate waves for theme: ' + 'all' );
-            setTimeout (function() {
-              $('.wave').show();
-              logger.info('\n' + 'initializing module finished');
-            }, {{template_config.page_on_load_timeout}} );
-          } else if (theme_enabled) {
-            logger.info('\n' + 'activate waves for theme: ' + theme );
-            setTimeout (function() {
-              $('.wave').show();
-              logger.info('\n' + 'initializing module finished');
-            }, {{template_config.page_on_load_timeout}} );
-          } else {
-            logger.warn('\n' + 'no valid theme/s found');
-            logger.warn('\n' + 'deactivate (hide) waves');
-            setTimeout (function() {
-              $('.wave').hide();
-              logger.info('\n' + 'initializing module finished');
-            }, {{template_config.page_on_load_timeout}} );
-          }
-
+          logger.info('\n' + 'initializing module finished');
           clearInterval(dependencies_met_page_ready);
         }
       }, 25);
