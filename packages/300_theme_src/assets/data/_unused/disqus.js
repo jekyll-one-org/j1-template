323799a1cc97a3b45862b6c3633159fcd0d9a771
Juergen Adams <juergen@jekyll.one>
2023-07-04 17:08:47 +0200

    Latest files of version 2023.3.3

---
+++ packages/300_theme_src/assets/data/_unused/disqus.js
@@ -0,0 +1,81 @@
+---
+regenerate:                             true
+---
+
+{% capture cache %}
+
+{% comment %}
+ # -----------------------------------------------------------------------------
+ # ~/assets/data/disqus.js
+ # Liquid PROCEDURE to generate the JS portion used by J1 Connector Disqus
+ #
+ # Product/Info:
+ # https://jekyll.one
+ #
+ # Copyright (C) 2023 Juergen Adams
+ #
+ # J1 Theme is licensed under the MIT License.
+ # See: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
+ # -----------------------------------------------------------------------------
+ # Test data:
+ #   liquid_var: {{ liquid_var | debug }}
+ #   config: {{ config | debug }}
+ # -----------------------------------------------------------------------------
+{% endcomment %}
+
+{% comment %} Liquid procedures
+-------------------------------------------------------------------------------- {% endcomment %}
+
+{% comment %} Set global settings
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign environment     = site.environment %}
+
+{% comment %} Set config files
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign modules         = site.data.modules %}
+{% assign j1_config       = site.data.j1_config %}
+
+{% comment %} Set variables
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign comment_provider = 'disqus' %}
+{% assign site_id          = j1_config.comments.disqus.site_id %}
+
+{% comment %} Main
+================================================================================ {% endcomment %}
+
+$(document).ready(function() {
+  var dependencies_met_page_finished = setInterval (function () {
+    if (j1.getState() === 'finished') {
+      var disqus_shortname = '{{site_id}}';
+      // -----------------------------------------------------------------
+      // RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE
+      // SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+      // LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT:
+      // https://disqus.com/admin/universalcode/#configuration-variables
+      // -----------------------------------------------------------------
+      //
+      // var disqus_config = function () {
+      // this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+      // Replace PAGE_IDENTIFIER with your page's unique
+      // identifier variable
+      // this.page.identifier = PAGE_IDENTIFIER;
+      // };
+      //
+      // DON'T EDIT BELOW THIS LINE
+      // -----------------------------------------------------------------
+      (function() {
+          var d = document,
+              s = d.createElement('script');
+          s.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+          s.setAttribute('data-timestamp', +new Date());
+          (d.head || d.body).appendChild(s);
+      })();
+      clearInterval(dependencies_met_page_finished);
+    }
+  }, 25);
+});
+
+{% endcapture %}
+
+{{ cache | strip_empty_lines }}
+{% assign cache = nil %}
---
+++ packages/300_theme_src/assets/data/_unused/hyvor.js
@@ -0,0 +1,72 @@
+---
+regenerate:                             true
+---
+
+{% capture cache %}
+
+{% comment %}
+ # -----------------------------------------------------------------------------
+ # ~/assets/data/disqus.js
+ # Liquid PROCEDURE to generate the JS portion used by J1 Connector Disqus
+ #
+ # Product/Info:
+ # https://jekyll.one
+ #
+ # Copyright (C) 2023 Juergen Adams
+ #
+ # J1 Theme is licensed under the MIT License.
+ # See: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE.md
+ # -----------------------------------------------------------------------------
+ # Test data:
+ #   liquid_var: {{ liquid_var | debug }}
+ #   config: {{ config | debug }}
+ # -----------------------------------------------------------------------------
+{% endcomment %}
+
+{% comment %} Liquid procedures
+-------------------------------------------------------------------------------- {% endcomment %}
+
+{% comment %} Set global settings
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign environment     = site.environment %}
+
+{% comment %} Set config files
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign modules         = site.data.modules %}
+{% assign j1_config       = site.data.j1_config %}
+
+{% comment %} Set variables
+-------------------------------------------------------------------------------- {% endcomment %}
+{% assign comment_provider = 'hyvor' %}
+{% assign site_id          = j1_config.comments.hyvor.site_id %}
+
+{% assign production = false %}
+{% if environment == 'prod' or environment == 'production' %}
+  {% assign production = true %}
+{% endif %}
+
+{% comment %} Main
+================================================================================ {% endcomment %}
+
+$(document).ready(function() {
+  var dependencies_met_page_finished = setInterval (function () {
+    if (j1.getState() === 'finished') {
+      // DON'T EDIT BELOW THIS LINE
+      // -----------------------------------------------------------------------
+      var site_id = {{site_id}};
+      (function() {
+        var HYVOR_TALK_WEBSITE = site_id;
+        var HYVOR_TALK_CONFIG = {
+            url: false,
+            id: false
+        };
+      })();
+    }
+    clearInterval(dependencies_met_page_finished);
+  }, 25);
+});
+
+{% endcapture %}
+
+{{ cache | strip_empty_lines }}
+{% assign cache = nil %}
