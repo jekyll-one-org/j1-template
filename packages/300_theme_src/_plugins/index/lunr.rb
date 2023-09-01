406459e8cc4fbf21dc8dfe9ad256ef0085afcdfa
Juergen Adams <juergen@jekyll.one>
2023-09-01 20:16:51 +0200

    New version 2023.7.5

--- packages/300_theme_src/_plugins/index/lunr.rb
+++ packages/300_theme_src/_plugins/index/lunr.rb
@@ -391,6 +391,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2023.7.4'
+    VERSION = '2023.7.5'
   end
 end
--- packages/500_theme_gem/lib/j1/version.rb
+++ packages/500_theme_gem/lib/j1/version.rb
@@ -1,3 +1,3 @@
 module J1
-  VERSION = '2023.7.4'
+  VERSION = '2023.7.5'
 end
