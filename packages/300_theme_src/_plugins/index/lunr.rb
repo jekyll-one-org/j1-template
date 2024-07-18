7f41e1beed2538af5834fbf8be47faef874b6160
Juergen Adams <juergen@jekyll.one>
2024-07-18 03:27:22 +0200

    New version 2024.3.1

--- packages/300_theme_src/_plugins/index/lunr.rb
+++ packages/300_theme_src/_plugins/index/lunr.rb
@@ -394,6 +394,6 @@ end
 
 module Jekyll
   module J1LunrSearch
-    VERSION = '2024.3.0'
+    VERSION = '2024.3.1'
   end
 end
--- packages/500_theme_gem/lib/j1/version.rb
+++ packages/500_theme_gem/lib/j1/version.rb
@@ -1,3 +1,3 @@
 module J1
-  VERSION = '2024.3.0'
+  VERSION = '2024.3.1'
 end
